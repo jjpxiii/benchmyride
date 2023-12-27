70765ba2eee1b504be427715fd1c67407f8e55f7
jjpxiii <jjpxiii@github.com>
2023-12-27 23:15:19 +0100

    update json bun

--- algorithms/json-serde/main_bun.ts
+++ algorithms/json-serde/main_bun.ts
@@ -1,13 +1,13 @@
 function printHash(data) {
   const str = JSON.stringify(data);
   const hasher = new Bun.CryptoHasher("md5");
-  const hash = hasher.update(str, "hex").digest();
+  const hash = hasher.update(str).digest("hex");
   console.log(hash);
 }
 
 async function main() {
   const fileName = "canada";
-  const jsonStr = await Bun.file(`${fileName}.json`).json();
+  const jsonStr = await Bun.file(`${fileName}.json`).text();
   printHash(JSON.parse(jsonStr));
   const array = [];
   for (let i = 0; i < 13; i++) {
