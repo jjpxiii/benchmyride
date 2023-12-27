56b028611213ed12e8a66610bb2f82c6a27d551e
jjpxiii <jjpxiii@github.com>
2023-12-27 23:05:01 +0100

    buuugs

--- algorithms/json-serde/main_bun.ts
+++ algorithms/json-serde/main_bun.ts
@@ -1,21 +1,20 @@
-function printHash(data: any) {
-    const str = JSON.stringify(data);
-    const hasher = new Bun.CryptoHasher("md5");
-    const hash = hasher.update(str, "hex").digest();
-    console.log(hash);
+function printHash(data) {
+  const str = JSON.stringify(data);
+  const hasher = new Bun.CryptoHasher("md5");
+  const hash = hasher.update(str, "hex").digest();
+  console.log(hash);
 }
 
 async function main() {
-    const fileName = "canada";
-    const jsonStr = await Bun.file(`${fileName}.json`).json();
-    printHash(JSON.parse(jsonStr));
-    const array = [];
-    for (let i = 0; i < 13; i++) {
-        array.push(JSON.parse(jsonStr));
-    }
-    printHash(array);
-    printHash(JSON.stringify(array));
-    
+  const fileName = "canada";
+  const jsonStr = await Bun.file(`${fileName}.json`).json();
+  printHash(JSON.parse(jsonStr));
+  const array = [];
+  for (let i = 0; i < 13; i++) {
+    array.push(JSON.parse(jsonStr));
+  }
+  printHash(array);
+  printHash(JSON.stringify(array));
 }
 
 main();
