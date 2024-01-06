a230f6f76bd22ffe8cda9f3e8731e5e32bad3931
jjpxiii <jjpxiii@github.com>
2024-01-06 18:07:10 +0100

    add js for json-serde

---
+++ algorithms/json-serde/main.mjs
@@ -0,0 +1,22 @@
+import { createHash } from "node:crypto";
+import { readFileSync } from "node:fs";
+
+function printHash(data) {
+  const str = JSON.stringify(data);
+  const hasher = createHash("md5");
+  const hash = hasher.update(str);
+  console.log(hash.digest("hex"));
+}
+
+async function main() {
+  const jsonStr = await readFileSync(`canada.json`, "utf8");
+  printHash(JSON.parse(jsonStr));
+  const array = [];
+  for (let i = 0; i < 13; i++) {
+    array.push(JSON.parse(jsonStr));
+  }
+  printHash(array);
+  printHash(JSON.stringify(array));
+}
+
+main();
