32029dd0bd83680cd6b1788a59e3b6afb5b690ba
jjpxiii <jjpxiii@github.com>
2024-02-06 16:22:11 +0100

    add native sort c#

---
+++ algorithms/native-sort/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json;
+
+var text = File.ReadAllText(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..", "10mil.json")));
+var arr = JsonSerializer.Deserialize<int[]>(text);
+Array.Sort(arr!);
+Console.WriteLine(arr![666]);
+Console.WriteLine(arr![666666]);
