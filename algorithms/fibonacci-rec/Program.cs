85e1a9e7e42cc0105ac05fca4ec3358e4d929ce0
JJP <jjpxiii@gmail.com>
2024-02-06 18:00:55 +0100

    add fibo c#

---
+++ algorithms/fibonacci-rec/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace fibo {
+    class Program {
+        public static int Fib(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+
+        public static void Main() {
+            Console.WriteLine(Fib(42));
+        }
+    }
+}
