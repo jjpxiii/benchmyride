f034b5eee0215d0742b3402022c7661b30e540b2
jjpxiii <jjpxiii@github.com>
2024-05-04 19:04:21 +0200

    improve c# regex

--- algorithms/regex/Program.cs
+++ algorithms/regex/Program.cs
@@ -1,23 +1,24 @@
-using System;
-using System.IO;
 using System.Text.RegularExpressions;
-using System.Diagnostics;
 
-class Benchmark
+partial class Benchmark
 {
+    [GeneratedRegex(@"[\w\.+-]+@[\w\.-]+\.[\w\.-]+")]
+    private static partial Regex Email();
+
+    [GeneratedRegex(@"[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?")]
+    private static partial Regex Uri();
+
+    [GeneratedRegex(@"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])")]
+    private static partial Regex IP();
     static void Main(string[] args)
     {
-        StreamReader reader = new System.IO.StreamReader("../../assets/regexin.txt");
+        StreamReader reader = new("../../assets/regexin.txt");
         string data = reader.ReadToEnd();
 
         // Email
-        Benchmark.Measure(data, @"[\w\.+-]+@[\w\.-]+\.[\w\.-]+");
-
-        // URI
-        Benchmark.Measure(data, @"[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?");
-
-        // IP
-        Benchmark.Measure(data, @"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])");
+        Console.WriteLine(Email().Matches(data).Count);
+        Console.WriteLine(Uri().Matches(data).Count);
+        Console.WriteLine(IP().Matches(data).Count);
     }
 
     static void Measure(string data, string pattern)
